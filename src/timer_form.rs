e476d8ea8bec9a027a2202d45490af3583321347
AlexKnauth <alexander@knauth.org>
2024-03-16 14:32:22 -0400

    update autosplitter settings when changing splits

--- src/timer_form.rs
+++ src/timer_form.rs
@@ -355,6 +355,7 @@ impl<T: Widget<MainState>> Widget<MainState> for WithMenu<T> {
                     let result = data.config.borrow_mut().open_splits(
                         &mut data.timer.write().unwrap(),
                         &mut data.layout_data.borrow_mut(),
+                        &data.auto_splitter,
                         file_info.path().to_path_buf(),
                     );
                     or_show_error(result);
